d16769a1b04f09c7cf411d1d7931b52f83968866
wojakpepechad <tokenalert7@gmail.com>
2024-02-10 18:27:46 +0100

    Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -10,60 +10,59 @@ const DextChart = () => {
         <iframe id="dextools-widget"
             title="DEXTools Trading Chart"
             width="500" height="400"
-            src="https://www.dextools.io/widget-chart/es/ether/pe-light/0x115f10b50314677f8e50189ff588533873e867e7?theme=light&chartType=2&chartResolution=30&drawingToolbars=false"></iframe>
+            src="https://www.dextools.io/widget-chart/es/ether/pe-light/0x115f10b50314677f8e50189ff588533873e867e7?theme=dark&chartType=1&chartResolution=30&drawingToolbars=false">
+        </iframe>
     )
 }
 
 const MY_TOKEN_LIST = [
     {
-    "name": "Wojak.farm",
-    "address": "0x4fd2EC9bDd398f8e522d76eA3704F8dBdc1f23f4",
-    "symbol": "WOJAK",
-    "decimals": 18,
-    "chainId": 1,
-    "logoURI": "https://pbs.twimg.com/profile_images/1312314914392993792/AEEJkpQM_400x400.jpg"
-  },
+        "name": "Wojak.farm",
+        "address": "0x4fd2EC9bDd398f8e522d76eA3704F8dBdc1f23f4",
+        "symbol": "WOJAK",
+        "decimals": 18,
+        "chainId": 1,
+        "logoURI": "https://pbs.twimg.com/profile_images/1312314914392993792/AEEJkpQM_400x400.jpg"
+    },
     {
-    "name": "Tether USD",
-    "address": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-    "symbol": "USDT",
-    "decimals": 6,
-    "chainId": 1,
-    "logoURI": "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png"
-  },
+        "name": "Tether USD",
+        "address": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+        "symbol": "USDT",
+        "decimals": 6,
+        "chainId": 1,
+        "logoURI": "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png"
+    },
 ]
 
 const UniSwapper = () => {
     const wojak = "0x4fd2EC9bDd398f8e522d76eA3704F8dBdc1f23f4";
-    
+
     // Define your custom token list URI (or use a predefined one)
     const customTokenListUri = MY_TOKEN_LIST;
-    
-    const widgetConfig = {	  
-      tokenList: customTokenListUri,
-      
-      // Other widget configuration options...
+
+    const widgetConfig = {
+        tokenList: customTokenListUri,
+
+        // Other widget configuration options...
     };
-  
+
     return (
-      <div className="Uniswap">
-        <SwapWidget {...widgetConfig} defaultOutputTokenAddress={wojak} />
-      </div>
+        <div className="Uniswap">
+            <SwapWidget {...widgetConfig} defaultOutputTokenAddress={wojak} />
+        </div>
     );
-  }
-  
+}
+
 
 
 
 const Info: NextPage = () => {
     return (
         <>
-            <Head>
-                <title>Info - wojak.farm</title>
-            </Head>
             <main>
-                <h1>Information Page</h1>
-                <p>This is the info page for wojak.farm.</p>
+            <Link href="/">
+                    <title>Wojak.farm</title>
+                </Link>
                 <div className={styles.chartSwapperContainer}>
                     <DextChart />
                     <UniSwapper />
